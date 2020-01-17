2a2149cf2d29ad672857f8689734bff908cf7c6f
PradyumnaDahal <pradyumnadahal2003@gmail.com>
2020-01-16 20:58:02 -0600

    Add DriveByEncoder script

---
+++ src/main/java/frc/robot/commands/DriveByEncoder.java
@@ -0,0 +1,71 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+import com.revrobotics.CANEncoder;
+import edu.wpi.first.wpilibj.Encoder;
+
+public class DriveByEncoder extends CommandBase{
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+    private double leftStartValue, rightStartValue;
+    private CANEncoder leftEncoder, rightEncoder;
+    private double distance;
+    private boolean leftFinished, rightFinished;
+
+    /**
+     * Creates a new DriveByEncoder Command.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public DriveByEncoder(double distance) {
+        addRequirements(RobotContainer.drivetrain);
+        leftEncoder = RobotContainer.drivetrain.motorEncoderL1;
+        rightEncoder = RobotContainer.drivetrain.motorEncoderR1;
+        this.distance = distance;
+      }
+
+        // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        RobotContainer.drivetrain.drive(0, 0); // Don't move on init
+        resetEncoders();
+        
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        
+        leftFinished = leftEncoder.getPosition() - leftStartValue >= distance;
+        rightFinished = leftEncoder.getPosition() - rightStartValue >= distance;
+
+        if(!leftFinished) {
+            RobotContainer.drivetrain.drive(0.3, 0);
+        }
+
+        if(!rightFinished) {
+            RobotContainer.drivetrain.drive(0, 0.3);
+        }
+
+        super.execute();
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(final boolean interrupted) {
+
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return leftFinished && rightFinished;
+    }
+
+    private void resetEncoders() {
+        leftStartValue = leftEncoder.getPosition();
+        rightStartValue = rightEncoder.getPosition();
+    }
+}

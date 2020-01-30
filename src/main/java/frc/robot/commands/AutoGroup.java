43c5810b1a1f57a915c69f730846f797315660d3
Henry Shires <hcsgeek@gmail.com>
2020-01-29 21:58:09 -0600

    Added SendableChooser for multiple auto commands

--- src/main/java/frc/robot/commands/AutoGroup.java
+++ src/main/java/frc/robot/commands/AutoGroup.java
@@ -4,27 +4,29 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutoGroup extends SequentialCommandGroup {
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
-    private int selection;
+    private String selection;
 
-    public AutoGroup(int selection) {
-        this.selection = selection;
+    public AutoGroup(String name) {
+        this.selection = name;
 
         switch (this.selection) {
             /* Trench Run */
-            case 1: addCommands(
-                        new DriveToPoint(0, -216),
-                        new DriveToPoint(0, -216)
-                    );
-                    break;
+            case "AUTO_1": 
+                addCommands(
+                    new DriveToPoint(0, -216),
+                    new DriveToPoint(0, -216)
+                );
+                break;
             /* Center to Mid */
-            case 2: addCommands(
-                new DriveToPoint(0, -105),
-                new DriveToPoint(-72, 0),
-                new DriveToPoint(-105, 0)
-            );
-
+            case "AUTO_2": 
+                addCommands(
+                    new DriveToPoint(0, -105),
+                    new DriveToPoint(-72, 0),
+                    new DriveToPoint(-105, 0)
+                );
+                break;
             /* Left to Mid */
-            // case 3: addCommands(
+            // case "AUTO_3": addCommands(
                 // new DriveToPoint(0, -115),
                 // new DriveToPoint(0, -115)
             // );

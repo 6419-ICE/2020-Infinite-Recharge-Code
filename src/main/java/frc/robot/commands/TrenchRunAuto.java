ce238428161ce6b4c17ce2943a8e1e4838ffcaa9
VeloticsTechnologies <hcsgeek@gmail.com>
2020-03-05 21:09:47 -0600

    Updated local branches

--- src/main/java/frc/robot/commands/TrenchRunAuto.java
+++ src/main/java/frc/robot/commands/TrenchRunAuto.java
@@ -8,7 +8,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.*;
 
 public class TrenchRunAuto extends SequentialCommandGroup {
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
